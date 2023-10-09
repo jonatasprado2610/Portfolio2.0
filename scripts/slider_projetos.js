a28856c9e7e49b25a76fb3941eaefdc187b892b3
jonatas da silva prado <jonatasprado445@gmail.com>
2023-10-09 15:25:23 -0300

    okay

---
+++ scripts/slider_projetos.js
@@ -0,0 +1,31 @@
+$(document).ready(()=>{
+    new Glider(document.querySelector('.glider'), {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        draggable: false,
+        dots: '.dots',
+        arrows: {
+            prev: '.glider-prev',
+            next: '.glider-next'
+        },
+        responsive:[
+            {
+                breakpoint: 640,
+                settings:{
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                }
+                
+            },
+            {
+                breakpoint: 1000,
+                settings:{
+                    slidesToShow: 3,
+                    slidesToScroll: 3
+                }
+                
+            }
+        ]
+    });
+
+})
